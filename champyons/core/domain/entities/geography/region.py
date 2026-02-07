46d24682d8f7ceff2d742d61d02390737e00f178
artveal <artveal1996@gmail.com>
2026-02-07 14:09:48 +0000

    region fixed

--- champyons/core/domain/entities/geography/region.py
+++ champyons/core/domain/entities/geography/region.py
@@ -1,6 +1,7 @@
 from champyons.core.domain.entities.mixins.geography import GeographyMixin
 from champyons.core.domain.entities.mixins.timestamp import TimestampMixin
 from champyons.core.domain.entities.mixins.active import ActiveMixin
+from champyons.core.domain.enums.region import RegionTypeEnum
 
 from dataclasses import dataclass, field
 from typing import TYPE_CHECKING, List, Optional
@@ -10,9 +11,10 @@ if TYPE_CHECKING:
 
 @dataclass
 class Region(ActiveMixin, GeographyMixin, TimestampMixin):
-    """ A supranational"""
+    """ A supranational entity thar contains nations. Can be used for geographics regions, treaties..."""
     id: Optional[int] = None
     name: str = ""
+    type: RegionTypeEnum = RegionTypeEnum.SCOUTABLE_REGION
 
     # Relationships
     nations: List["Nation"] = field(default_factory=list)
