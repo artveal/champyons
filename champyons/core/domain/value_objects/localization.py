a320645fd0e44053d958b9975eb75b30321faa24
artveal <artveal1996@gmail.com>
2026-02-08 16:17:48 +0000

    classmethod for default Localization is added. Lazy string added for lazy translations

--- champyons/core/domain/value_objects/localization.py
+++ champyons/core/domain/value_objects/localization.py
@@ -1,8 +1,40 @@
 from dataclasses import dataclass
 from zoneinfo import ZoneInfo
+from typing import Callable
 
 @dataclass(frozen=True)
 class LocalizationContext:
     language: str
     locale: str
     timezone: ZoneInfo
+
+    @classmethod
+    def default(cls) -> "LocalizationContext":
+        return cls(
+            language="en",
+            locale="en_US",
+            timezone=ZoneInfo("UTC")
+        )
+    
+class LazyString:
+    """A string-like object that evaluates the translation only when needed."""
+    def __init__(self, func: Callable[[], str]):
+        self._func = func
+
+    def __str__(self):
+        return self._func()
+
+    def __repr__(self):
+        return f"LazyString({str(self)})"
+
+    def __add__(self, other):
+        return str(self) + str(other)
+
+    def __radd__(self, other):
+        return str(other) + str(self)
+
+    def __mod__(self, other):
+        return str(self) % other
+
+    def format(self, *args, **kwargs):
+        return str(self).format(*args, **kwargs)
