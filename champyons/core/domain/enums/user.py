f5ef03a7bcbc1ace260c260930ac3868ea019796
Arturo Vega <artveal1996@gmail.com>
2026-02-11 21:14:06 +0100

    documentation

--- champyons/core/domain/enums/user.py
+++ champyons/core/domain/enums/user.py
@@ -1,5 +1,5 @@
 from enum import StrEnum
 
 class UserRoleEnum(StrEnum):
-    """User roles available """
-    ADMIN = "admin"
+    """User roles available. """
+    ADMIN = "admin" # More roles ro be added in the fuure as the game develops
